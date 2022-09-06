2107c9bfc4ac5d36c15611d4d5cca87dd060f556
santoshmondal <santosh.ece06@gmail.com>
2022-09-06 11:10:59 +0530

    CORS

--- src/main/java/in/cdac/controller/UserModelController.java
+++ src/main/java/in/cdac/controller/UserModelController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,9 @@ import in.cdac.service.UserModelService;
 
 import java.util.List;
 
+
 @Validated
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserModelController {
